cba879bb3e82dee04600a968341a69db10515d18
MatTerra <mateus.b.s.terra@gmail.com>
2020-10-23 10:16:14 -0300

    Mark parametrize test

--- test/unittests/test_api.py
+++ test/unittests/test_api.py
@@ -1,6 +1,6 @@
 from unittest.mock import Mock
 
-from pytest import fixture
+from pytest import fixture, mark
 
 from User import User
 from app import user_api
@@ -32,6 +32,27 @@ class TestAPI:
         a = user_api.read.__wrapped__(dao=dao_mock)
 
         assert dao_mock.get_all.called_with(length=20, offset=0, filters=None)
+        assert a == ('List of user', {"total": len(users),
+                                      "results": [dict(user)
+                                                  for user
+                                                  in users]})
+
+    @mark.parametrize("length, offset, kwargs, filters", [
+        (10, 0, {"name": "MyName"}, {"name": "MyName"}),
+        (70, 0, {}, None),
+        (2, 0, {"name": "LIKE,My%"}, {"name": ["LIKE", "My%"]})
+    ])
+    def test_read_with_params(self, success_response, dao_mock,
+                              length, offset, kwargs, filters):
+        users = [User(name="MyName"), User(name="OtherName")]
+
+        dao_mock.get_all.return_value = len(users), users
+
+        a = user_api.read.__wrapped__(length=length, offset=offset,
+                                      dao=dao_mock, **kwargs)
+
+        assert dao_mock.get_all.called_with(length=length, offset=offset,
+                                            filters=filters)
         assert a == ('List of user', {"total": len(users),
                                       "results": [dict(user)
                                                   for user
